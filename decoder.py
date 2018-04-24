fa340e6aaf2aae6652f90fdb2b0c9fc023e742e9
paul.kuznecov <mail@paul-kuznecov.de>
2018-04-24 21:15:59 +0200

    Initial commit

---
+++ decoder.py
@@ -0,0 +1,109 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+MAX_LENGTH = 10
+use_cuda = False
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        result = Variable(torch.zeros(1, 1, self.hidden_size))
+        if use_cuda:
+            return result.cuda()
+        else:
+            return result
+
+######################################################################
+# I encourage you to train and observe the results of this model, but to
+# save space we'll be going straight for the gold and introducing the
+# Attention Mechanism.
+#
+
+
+######################################################################
+# Attention Decoder
+# ^^^^^^^^^^^^^^^^^
+#
+# If only the context vector is passed betweeen the encoder and decoder,
+# that single vector carries the burden of encoding the entire sentence.
+#
+# Attention allows the decoder network to "focus" on a different part of
+# the encoder's outputs for every step of the decoder's own outputs. First
+# we calculate a set of *attention weights*. These will be multiplied by
+# the encoder output vectors to create a weighted combination. The result
+# (called ``attn_applied`` in the code) should contain information about
+# that specific part of the input sequence, and thus help the decoder
+# choose the right output words.
+#
+# .. figure:: https://i.imgur.com/1152PYf.png
+#    :alt:
+#
+# Calculating the attention weights is done with another feed-forward
+# layer ``attn``, using the decoder's input and hidden state as inputs.
+# Because there are sentences of all sizes in the training data, to
+# actually create and train this layer we have to choose a maximum
+# sentence length (input length, for encoder outputs) that it can apply
+# to. Sentences of the maximum length will use all the attention weights,
+# while shorter sentences will only use the first few.
+#
+# .. figure:: /_static/img/seq-seq-images/attention-decoder-network.png
+#    :alt:
+#
+#
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
+
+        attn_weights = F.softmax(
+            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
+
+    def initHidden(self):
+        result = Variable(torch.zeros(1, 1, self.hidden_size))
+        if use_cuda:
+            return result.cuda()
+        else:
+            return result
